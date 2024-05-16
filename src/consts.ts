7789d90498c633c6350c0608b9aa20aebc96a1d2
Juan <arzzdeveloper04@gmail.com>
2024-05-16 12:37:40 -0600

    new_update

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,5 @@ export const SITE_TITLE = 'JUAN ARVIZU';
 export const SITE_DESCRIPTION = 'Bienvenido a la pagina de reportes de practica!';
 
 export const FIGURAS_DESCRIPTION = "El objetivo es entender los conceptos de abstracción, herencia y polimorfismo, así como su aplicación en la resolución de problemas relacionados con el cálculo de áreas y perímetros de diversas figuras geométricas.";
+export const PERSONAS_DESCRIPTION = "El objetivo es entender los conceptos de abstracción, herencia y polimorfismo, así como su aplicación en la resolución de problemas relacionados con la gestión de personas en una organización por medio de script de terminal.";
+export const BANCO_DESCRIPTION = "El objetivo es entender los conceptos de abstracción, herencia y polimorfismo, así como su aplicación en la resolución de problemas relacionados con la gestión de cuentas bancarias por medio de script de terminal.";
